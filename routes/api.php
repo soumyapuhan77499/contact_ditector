d84bd27d768820a4e81a209017fb58dc3dda4992
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-02-22 13:27:22 +0530

    Fix namespace casing for ContactApiController in routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\api\ContactApiController;
+use App\Http\Controllers\Api\ContactApiController;
 
 Route::post('/save-contact', [ContactApiController::class, 'saveContact']);
 
